4b15e2318438974516b8139404a0eff837a8b187
Shlok Patel <shlokcpatel2001@gmail.com>
2022-01-01 13:37:41 +0530

    First commit

---
+++ src/deploy-commands.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const dotenv = require('dotenv').config()
+const clientId = process.env.CLIENTID
+const token = process.env.TOKEN
+const guildId = process.env.GUILDID
+console.log(clientId, token, guildId)
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
